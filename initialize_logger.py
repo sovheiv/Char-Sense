cc82a4ece17cf1c9d0d918a22aa5f638b94dc8f9
best_bot_developer <55vladimir5@gmail.com>
2021-10-04 23:56:26 +0300

    improve logging, bugfix

--- initialize_logger.py
+++ initialize_logger.py
@@ -1,23 +1,32 @@
 import logging
-from logging import Formatter
+from logging import Formatter, StreamHandler
 
+
+class StreamHandlerErrorsOnly(StreamHandler):
+    def emit(self, record):
+        if record.levelno != logging.ERROR:
+            return
+        super().emit(record)
+
+class StreamHandlerInfoOnly(StreamHandler):
+    def emit(self, record):
+        if record.levelno != logging.INFO:
+            return
+        super().emit(record)
 
 info_log_format = "%(message)s"
 error_log_format = "[%(levelname)s]: %(message)s"
 
-info_logger = logging.getLogger(name="penguin")
-info_logger.setLevel(logging.INFO)
+output_logger = logging.getLogger(name="penguin")
+output_logger.setLevel(logging.INFO)
 
-error_logger = logging.getLogger(name="kaban")
-error_logger.setLevel(logging.ERROR)
-
-info_console_handler = logging.StreamHandler()
-info_console_handler.setLevel(logging.INFO)
-info_console_handler.setFormatter(Formatter(info_log_format))
-
-error_console_handler = logging.StreamHandler()
+error_console_handler = StreamHandlerErrorsOnly()
 error_console_handler.setLevel(logging.ERROR)
 error_console_handler.setFormatter(Formatter(error_log_format))
 
-error_logger.addHandler(error_console_handler)
-info_logger.addHandler(info_console_handler)
+info_console_handler = StreamHandlerInfoOnly()
+info_console_handler.setLevel(logging.INFO)
+info_console_handler.setFormatter(Formatter(info_log_format))
+
+output_logger.addHandler(error_console_handler)
+output_logger.addHandler(info_console_handler)
